e1f9ae5daf7607bd74fdce5860bebf36b7b4694e
unknown <mcbrider5002@gmail.com>
2018-11-14 02:34:22 +0000

    Spectra plot now plots spikes rather than incorrect line.

--- project/spectra/drawSpectra.py
+++ project/spectra/drawSpectra.py
@@ -48,9 +48,14 @@ def drawSpectrum(spectrum, tag, colour_key=default_colours):
 		ax.annotate(s='', xy=(mass2,tag_height), xytext=(mass1,tag_height), arrowprops=dict(arrowstyle='<->', shrinkA=0, shrinkB=0, color=colour_key.get(compound, "b")))
 		ax.text(mass1, text_height, compound)
 		
-	ax.plot(spectrum.ms2peaks[:, MassSpectrum.MASS], spectrum.ms2peaks[:, MassSpectrum.INTENSITY], color="black")
+	'''for mass, intensity in zip(spectrum.ms2peaks[:, MassSpectrum.MASS], spectrum.ms2peaks[:, MassSpectrum.INTENSITY]):
+		ax.plot(np.array([mass, mass]), np.array([0, intensity]), color="black")'''
+		
+	for mass, intensity in zip(spectrum.ms2peaks[:, MassSpectrum.MASS], spectrum.ms2peaks[:, MassSpectrum.INTENSITY]):
+		ax.plot(np.array([mass, mass]), np.array([0, intensity]), color="black")
 
 	ax.set(xlabel="Mass (m/z)", ylabel="Intensity", title=spectrum.id)
+	ax.set_ylim(ymin=0)
 
 	plt.show()
 	
